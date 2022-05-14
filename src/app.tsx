706a0a29a58ee8359462d2d49c8d52ae447dfd9a
roberte777 <robewilkes@yahoo.com>
2022-05-14 09:33:41 -0500

    init

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Main from "./components/Main";
+import "./index.css";
+
+function render() {
+    ReactDOM.render(<Main />, document.body);
+}
+render();
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./main.css";
+
+function Main() {
+    return <div className="app"> Hello world </div>;
+}
+export default Main;
